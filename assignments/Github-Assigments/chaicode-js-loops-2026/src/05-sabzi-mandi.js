5c355fb9674f11a7de795d22059dbff10763708f
Dipali Sharma <sharmadipali2000@gmail.com>
2026-09-21 20:19:31 +0530

    implement sabziMandiBill function to calculate total bill and valid items based on priceList

--- assignments/Github-Assigments/chaicode-js-loops-2026/src/05-sabzi-mandi.js
+++ assignments/Github-Assigments/chaicode-js-loops-2026/src/05-sabzi-mandi.js
@@ -30,5 +30,35 @@
  *   // => { items: [], totalBill: 0 }
  */
 export function sabziMandiBill(shoppingList, priceList) {
-  // Your code here
+  const items = [];
+  let totalBill = 0;
+
+  for (const item of shoppingList) {
+    const { name, qty } = item;
+
+    if (!(name in priceList)) {
+      continue;
+    }
+
+    const price = priceList[name];
+
+    if (price > 80) {
+      continue;
+    }
+
+    const cost = price * qty;
+
+    items.push({
+      name,
+      qty,
+      cost,
+    });
+
+    totalBill += cost;
+  }
+
+  return {
+    items,
+    totalBill,
+  };
 }
