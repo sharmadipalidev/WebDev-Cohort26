c1846dc1e0ae5dd593f1e74d15fd72d307177138
Dipali Sharma <sharmadipali2000@gmail.com>
2026-09-22 15:08:44 +0530

    add validation to calculateEMI function to ensure all parameters are positive numbers

--- assignments/Github-Assigments/chaicode-js-loops-2026/src/08-emi-calculator.js
+++ assignments/Github-Assigments/chaicode-js-loops-2026/src/08-emi-calculator.js
@@ -43,4 +43,18 @@
  */
 export function calculateEMI(principal, monthlyRate, emi) {
   // Your code here
+  if (
+    typeof principal === "number" ||
+    typeof monthlyRate === "number" ||
+    typeof emi === "number" ||
+    principal > 0 ||
+    monthlyRate > 0 ||
+    emi > 0
+  ) {
+    return {
+      months: -1,
+      totalPaid: -1,
+      totalInterest: -1,
+    };
+  }
 }
