a6e704b84046f4f0c46d141da3a0e937d276102e
Dipali Sharma <sharmadipali2000@gmail.com>
2026-09-24 19:09:00 +0530

    implement calculateDosaOrder function with pricing and validation logic

--- assignments/Github-Assigments/chaicode-js-functions-2026/src/01-dosa-counter.js
+++ assignments/Github-Assigments/chaicode-js-functions-2026/src/01-dosa-counter.js
@@ -33,4 +33,27 @@
  */
 export function calculateDosaOrder(type, quantity = 1, isSpicy = false) {
   // Your code here
+  const prices = {
+    plain: 40,
+    masala: 60,
+    onion: 50,
+    butter: 70,
+    paper: 90,
+    cheese: 80,
+  };
+  if (typeof type !== "string" || !prices[type]) return null;
+
+  if (!Number.isInteger(quantity) || quantity <= 0 || quantity === NaN)
+    return null;
+
+  const basePrice = prices[type];
+  const pricePerDosa = basePrice + (isSpicy ? 10 : 0);
+  const total = pricePerDosa * quantity;
+
+  return {
+    type,
+    quantity,
+    pricePerDosa,
+    total,
+  };
 }
